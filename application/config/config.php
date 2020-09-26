64c7ecdf2836a6db3c69ff1b872b0e250a634718
Justin <justinsang15@gmail.com>
2020-09-26 13:44:44 +0700

    Fix Connect DB with Azure

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/stock/';
+$config['base_url'] = 'http://localhost:52475/stock/';
 
 /*
 |--------------------------------------------------------------------------
