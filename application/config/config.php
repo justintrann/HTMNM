f44515149672e901d030aa0b40afed10805fb906
Justin <justinsang15@gmail.com>
2020-10-02 00:46:50 +0700

    Fixa

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'https://4401104182.azurewebsites.net';
+$config['base_url'] = 'http://localhost/';
 
 /*
 |--------------------------------------------------------------------------
