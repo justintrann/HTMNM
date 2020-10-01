9862eb82f4037ac605052c898c304e404f1a2995
Justin <justinsang15@gmail.com>
2020-10-02 00:06:57 +0700

    Fixxx

--- application/config/database.php
+++ application/config/database.php
@@ -153,10 +153,33 @@ $db['default'] = array(
 	'stricton' => FALSE,
 	'failover' => array(),
 	'save_queries' => TRUE
+	
+
+	define('ENV_STR', 'MYSQLCONNSTR_localdb');
+
+$return = array('result' => false);
+
+if (isset($_SERVER[ENV_STR])) {
+    $connectStr = $_SERVER[ENV_STR];
+
+    $return['connection'] = array(
+        'host' => preg_replace("/^.*Data Source=(.+?);.*$/", "\\1", $connectStr),
+        'database' => preg_replace("/^.*Database=(.+?);.*$/", "\\1", $connectStr),
+        'user' => preg_replace("/^.*User Id=(.+?);.*$/", "\\1", $connectStr),
+        'password' => preg_replace("/^.*Password=(.+?)$/", "\\1", $connectStr)
+    );
+
+    $return['result'] = true;
+}
+
+header('Content-Type: application/json; charset=utf-8');
+echo json_encode($return);
 );
 
-/*Snippet for PHP (PDO)*/
 
+
+/*Snippet for PHP (PDO)*/
+/*
 define('CONN_HOST', getenv("localhost"));
 define('CONN_DATABASE', getenv("stock"));
 define('CONN_USER', getenv("azure"));
@@ -165,3 +188,4 @@ define('CONN_OPTION', array(
 	PDO::MYSQL_ATTR_SSL_CA          =>     '/home/site/wwwroot/cert/BaltimoreCyberTrustRoot.crt.pem',
 	PDO::MYSQL_ATTR_INIT_COMMAND    => "SET NAMES utf8"
 ));
+*/
